1650a314ff76f88cd69d79f131d0f9e829f30735
jehyuck <zzukk78@naver.com>
2021-08-01 00:52:10 +0900

    Quiz 7월 30일 4조 과제완료

--- src/day06_while/Quiz05.java
+++ src/day06_while/Quiz05.java
@@ -1,16 +1,59 @@
 package day06_while;
 
+import java.util.Scanner;
+
 public class Quiz05 {
 	public static void main(String[] args) {
 		//문제 1번
+		/*
+		int rice=100000, mouse=2, day;
+		for(day=1; rice>0; day++) {
+			rice-=mouse*20;
+			if(day%10==0) {
+				mouse *=2;
+			}
+		}
+		day--;
+		System.out.println(day+"일"+mouse+"마리");
+		*/
 		//문제 2번
+		/*
+		Scanner input = new Scanner(System.in);
+		int num1, num2, max=0, min=0, i=2;
+		System.out.println("두 수 입력:");
+		num1=input.nextInt();
+		num2=input.nextInt();
+		while(i<=num1&&i<=num2) {
+			if(num1%i==0 && num2%i==0)
+				min=i;
+				i++;
+		}
+		max=num1*num2/min; //최소 공배수 구하는 방법
+		System.out.println("최소 공배수 : "+max);
+		System.out.println("최대 공약수 : "+min);
+		*/
 		//문제 3번
+		/*
+		Scanner input = new Scanner(System.in);
+		int num, cnt=1;
+		System.out.println("수 입력 : ");
+		num =input.nextInt();
+		while(true) {
+			if(num%cnt==0)
+				System.out.println(cnt+" ");
+			if(num==cnt)
+				break;
+			cnt++;
+		}
+		*/
 		//문제 4번
+		/*
 		int i=1,sum=0;
 		for(i=1; i<=1000; i++) {
 			if(i%4==1 && i%6==1) {
 				sum+=i;
 			}
 		}System.out.println("나머지가 1인 수의 합 : "+sum);
+		*/
 	}
 }
--- src/day06_while/Quiz06_team4.java
+++ src/day06_while/Quiz06_team4.java
@@ -1,14 +1,75 @@
 package day06_while;
 
+import java.util.Scanner;
+
 public class Quiz06_team4 {
 	public static void main(String[] args) {
 		//문제 1번 별 찍기
+		/*
 		for (int i = 5; i > 0; i--) {
 			for (int j = 0; j < i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
+		*/
+		//문제 2번
+		/*
+		for(int hour=1; hour<=23; hour++) {
+			for(int min=0; min<=59; min++) {
+				System.out.println(hour+"시"+min+"분");
+			}
+		}
+		*/
+		//문제 3번
+		/*
+		Scanner input=new Scanner(System.in);
+		int n=1;
+		System.out.println("수를 입력하시오 : ");
+		n=input.nextInt();
+		for(;n>=1 ; n--) {
+			int sum=0;
+			int k=n/2;
+			
+			for(int j=1; j<=k; j++) {
+				if(n%j==0) 
+					sum=sum+j;
+			}
+			if(n==sum) {
+				System.out.print
+				("완전수"+n+" = ");
+				int h =n/2;
+				
+				for(int l=1; l<=h; l++)
+					if(n%l==0)
+						System.out.print("약수:"+l+",");
+				System.out.println("");
+			}
+		}
+		*/
+		//문제 4번
+		/*
+		for(int i=0; i<5; i++)
+		{
+			for(int j=0; j<=3-i; j++)
+			{
+				System.out.print(" ");
+			}
+			for(int k=0; k<=2*i; k++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		*/
+		//문제 5번
+		/*
+		for(int i=1; i<=10; i++) {
+			for(int j=1; j<=10; j++) {
+				System.out.print("*");
+			}System.out.println();
+		}
+		*/
 
 	}
 }
